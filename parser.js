81698b73ccc01eebf488c1a7aeb85093c8952426
alex <bloodhound1982@gmail.com>
2017-11-25 13:28:51 +0200

    parsed and save links

--- parser.js
+++ parser.js
@@ -1,10 +1,28 @@
-const { compose, split, tap, map, reject, isEmpty, prop } = require('ramda')
+const { pluck, pathOr, curry, compose, composeP, split, map, reject, isEmpty, prop } = require('ramda')
 const axios = require('axios')
 const parser = require('rss-parser')
 const { promisify } = require('util')
+const path = require('path')
 const fs = require('fs')
 const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
 const parseString = promisify(parser.parseString)
+
+const { createFolderIfExist } = require('./services')
+
+const createFolderForVideos = curry((filePath, data) => {
+  const { dir } = path.parse(filePath)
+  const destinationDir = `${dir}/${pathOr('Unnamed', ['feed', 'title'], data)}`
+  return createFolderIfExist(destinationDir, data)
+})
+
+const writeDataToDestinationFolder = ([destinationDir, data]) =>
+  writeFile(`${destinationDir}/links.json`, JSON.stringify(data), 'utf8')
+
+const parseFeed = ([destinationDir, feed]) => {
+  const links = compose(map(prop('url')), pluck('enclosure'), pathOr([], ['feed', 'entries']))(feed)
+  return [destinationDir, links]
+}
 
 const getRSS = link =>
   axios.get(link)
@@ -13,8 +31,8 @@ const getRSS = link =>
 const parseRSSFile = filePath =>
   readFile(filePath, 'utf8')
     .then(compose(reject(isEmpty), split(/\n/)))
-    .then(links => Promise.all(map(getRSS, links)))
-    .then(tap(console.log))
+    .then(links => Promise.all(map(composeP(writeDataToDestinationFolder, parseFeed, createFolderForVideos(filePath), getRSS))(links)))
+    // .then(map(parseFeed))
 
 module.exports = {
   parseRSSFile,
--- services.js
+++ services.js
@@ -1,13 +1,32 @@
-const { compose, tap } = require('ramda')
+const { last, split, tap, compose, ifElse } = require('ramda')
+const fs = require('fs')
+const { promisify } = require('util')
+const mkdir = promisify(fs.mkdir)
 
 const log = (message, CODE = 0) => {
-  console.log(`****** ${message} ******`)
+  console.log(`****** ${message}`)
   return CODE
+}
+
+const infoMessage = (operation, dirName) => {
+  const folderName = compose(last, split('/'))(dirName)
+  console.log(`****** Log: Folder '${folderName}' is ${operation}.`)
+  return Promise.resolve([operation, dirName])
 }
 
 const exitWithMessage = compose(process.exit, log)
 
+const createFolderIfExist = ifElse(
+  (dir) => fs.existsSync(dir),
+  (dir, data) => Promise.resolve([dir, data])
+    .then(tap(() => infoMessage('existed', dir))),
+  (dir, data) => mkdir(dir)
+    .then(() => tap(infoMessage('created', dir)))
+    .then(() => [dir, data])
+)
+
 module.exports = {
   log,
   exitWithMessage,
+  createFolderIfExist,
 }
