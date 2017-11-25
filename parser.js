efef8937a0f79cd64e16abe315002a89ec4efe77
alex <bloodhound1982@gmail.com>
2017-11-25 19:52:35 +0200

    succes parsing all lists

--- parser.js
+++ parser.js
@@ -1,4 +1,5 @@
-const { toString, pluck, pathOr, curry, compose, composeP, split, map, reject, isEmpty, prop, tap } = require('ramda')
+const { addIndex, last, pluck, pathOr, curry, compose, composeP, split, map, reject, isEmpty,
+        prop, tap, nth, concat, __ } = require('ramda')
 const axios = require('axios')
 const parser = require('rss-parser')
 const { promisify } = require('util')
@@ -7,6 +8,7 @@ const fs = require('fs')
 const readFile = promisify(fs.readFile)
 const writeFile = promisify(fs.writeFile)
 const parseString = promisify(parser.parseString)
+const mapIndexed = addIndex(map)
 
 const { createFolderIfExist } = require('./services')
 
@@ -28,7 +30,7 @@ const getRSS = link =>
   axios.get(link)
     .then(compose(parseString, prop('data')))
 
-const getFile = curry((destinationDir, link) =>
+const getFile = curry((destinationDir, link, idx) =>
   axios.request({
     url: link,
     method: 'get',
@@ -38,15 +40,31 @@ const getFile = curry((destinationDir, link) =>
     }
   })
     .then(prop('data'))
-    .then(fileData => writeFile(`${destinationDir}/${toString(Date.now())}`, fileData))
-    .catch(err => {
-      console.error(err)
-      return true
+    .then(fileData => {
+      const formattedIdx = idx > 8 ? `${idx + 1}-` : `0${idx + 1}-`
+      const courseDir = compose(concat(__, '/'), last, split('/'))(destinationDir)
+      const fileName = compose(concat(__, '.mp4'), concat(formattedIdx), nth(4), split('/'))(link)
+      console.log(`****** ${fileName} is downloaded.`)
+      return writeFile(`${destinationDir}/${fileName}`, fileData)
+        .then(() => {
+          console.log(`      ${concat(courseDir, fileName)} is saved.`)
+          return '\n' + fileName
+        })
+        .catch(err => {
+          console.log(`      ${concat(courseDir, fileName)} isn't saved.`)
+          return 'fault'
+        })
     })
+    // .then(fileData => writeFile(`${destinationDir}/${toString(Date.now())}`, fileData))
+    // .catch(err => {
+    //   console.error(err)
+    //   return true
+    // })
 )
 
 const getFilesFromEgghead = ([destinationDir, links]) =>
-  Promise.all(map(getFile(destinationDir))(links))
+  Promise.all(mapIndexed(getFile(destinationDir))(links))
+
 
 const parseRSSFile = filePath =>
   readFile(filePath, 'utf8')
